95a2eba13d8568c0678ae70927e7e72ecf182328
Lukasz <intergenixyt@gmail.com>
2021-03-18 21:11:43 +0100

    basic sockets

--- lib/repositories/connectivity_repository.dart
+++ lib/repositories/connectivity_repository.dart
@@ -1,9 +1,12 @@
 import 'package:footboard/data_providers/connectivity_data_provider.dart';
 import 'package:footboard/models/connectivity_status.dart';
 import 'package:footboard/utils/service_locator.dart';
+import 'package:web_socket_channel/io.dart';
 
 class ConnectivityRepository {
   final ConnectivityDataProvider _connectivityDataProvider = sl();
+
+  IOWebSocketChannel? channel;
 
   Stream<ConnectivityStatus> get connectivityStatusStream {
     return _connectivityDataProvider.connectivityStatusStream;
@@ -20,6 +23,34 @@ class ConnectivityRepository {
   // TODO(genix): remove
   void setConnectivityStatus(ConnectivityStatus connectivityStatus) {
     _connectivityDataProvider.setConnectivityStatus(connectivityStatus);
+
+    if (connectivityStatus == ConnectivityStatus.disconnected) {
+      channel?.sink.close();
+    }
+
+    if (connectivityStatus == ConnectivityStatus.connecting) {
+      channel?.sink.close();
+      channel = IOWebSocketChannel.connect('ws://localhost:8080/create');
+
+      channel!.stream.listen(
+        (dynamic message) {
+          print('=== NEW MESSAGE ===');
+          print(message);
+        },
+        onError: (dynamic error) {
+          print('== ON ERROR ==');
+          print(error);
+        },
+        onDone: () {
+          // Executed when connection gets closed.
+          print('== ON DONE ==');
+        },
+      );
+    }
+
+    if (connectivityStatus == ConnectivityStatus.connected) {
+      channel?.sink.add('AAA');
+    }
   }
 
   Future<bool> host() async {
